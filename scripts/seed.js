b4e74e20a49a96f1d301e8ae7837ed298fb15890
Gabriel Cordeiro <gabrielcemp@gmail.com>
2024-05-07 18:11:44 -0300

    add script to insert mock data into the database

---
+++ scripts/seed.js
@@ -0,0 +1,26 @@
+const { mockData } = require("./mock-data");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await Promise.all(
+    mockData.map(async (job) => {
+      await prisma.job.upsert({
+        where: { slug: job.slug },
+        update: job,
+        create: job,
+      });
+    }),
+  );
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error("Error while seeding database: ", e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
